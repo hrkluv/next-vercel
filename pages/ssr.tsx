ac105df2a34fe89cd35fe5004ba81a131b4b148b
hrkluv <hrk.luv@gmail.com>
2022-03-17 10:04:40 +0900

    SSRページ usestate

--- pages/ssr.tsx
+++ pages/ssr.tsx
@@ -1,12 +1,28 @@
+import { GetServerSideProps } from "next"
+import { useState } from "react"
+
 export default function SSR({ random }: {random: number}) {
+  const [num, setNum] = useState(random)
   return (
     <div>
       <p>SSRで生成されたページ</p>
-      <em>{random}</em>
+      <em>{num}</em>
+      <button type="button" onClick={() => setNum(num + 1)}>change</button>
     </div>
   )
 }
 
+export const getServerSideProps: GetServerSideProps = async () => {
+  const random = Math.floor( Math.random() * 100 )
+
+  return {
+    props: {
+      random
+    }
+  }
+}
+
+/*
 export async function getServerSideProps() {
   const random = Math.floor( Math.random() * 100 )
 
@@ -16,3 +32,4 @@ export async function getServerSideProps() {
     }
   }
 }
+*/
